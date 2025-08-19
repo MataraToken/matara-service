67dfc6a0b716c150d4feae7bbd85c056813d6332
Aniebiet Obo <aniebietabasiobo@gmail.com>
2025-08-19 08:01:11 +0100

    commit

--- dist/bot/index.js
+++ dist/bot/index.js
@@ -61,8 +61,8 @@ bot.start(async (ctx) => {
         }
         const profilePicture = await getProfilePicture(ctx.from.id);
         // Use your actual web app URL here instead of ngrok
-        // const weburl = process.env.WEB_APP_URL || "https://matara-tma.vercel.app/";
-        const weburl = "https://jurstadev.xyz/";
+        const weburl = process.env.WEB_APP_URL || "https://matara-tma.vercel.app/";
+        // const weburl =  "https://jurstadev.xyz/";
         const imageUrl = "https://res.cloudinary.com/wallnet/image/upload/t_new-mat/v1743246776/MATARA_kqx0kj.png";
         console.log(`Registering user: ${username}`);
         const res = await axios_1.default.post(`${serverUrl}/api/user/register`, {
