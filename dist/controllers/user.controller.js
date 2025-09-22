d37b6d52093b6f7f5f10121af4444abe53db60c6
Aniebiet Obo <aniebietabasiobo@gmail.com>
2025-09-22 22:46:44 +0100

    commit

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -27,11 +27,11 @@ const registerUser = async (req, res) => {
             profilePicture,
             firstName,
         });
-        const initialPoints = 20;
-        const additionalPoints = 10;
+        const initialPoints = 1000;
+        const additionalPoints = 500;
         const newPoints = new points_model_1.default({
             userId: newUser._id,
-            points: initialPoints + additionalPoints,
+            points: initialPoints,
         });
         if (referralCode) {
             const referredBy = await user_model_1.default.findOne({ referralCode }).session(session);
