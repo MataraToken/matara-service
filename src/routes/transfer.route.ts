c95a780be2dfe7218de42cc42a297329746ed653
Aniebiet Obo <aniebietabasiobo@gmail.com>
2025-11-22 21:51:50 +0100

    commit

--- src/routes/transfer.route.ts
+++ src/routes/transfer.route.ts
@@ -3,13 +3,13 @@ import {
   sendTokensToUser,
   sendTokensToExternal,
 } from "../controllers/transfer.controller";
-import { isAdmin } from "../middleware/admin";
+// import { isAdmin } from "../middleware/admin";
 
 const router = Router();
 
 // Admin-only routes for sending tokens
-router.post("/user", isAdmin, sendTokensToUser);
-router.post("/external", isAdmin, sendTokensToExternal);
+router.post("/user", sendTokensToUser);
+router.post("/external", sendTokensToExternal);
 
 export default router;
 
